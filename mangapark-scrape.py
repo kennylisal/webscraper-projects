a90fb642b14c97580879bcccbdb5d009dda383db
kennylisal <kennylisal5@gmail.com>
2025-10-03 11:16:44 +0800

    comp[lete all basic function for scrape + improved error handling structure

--- mangapark-scrape.py
+++ mangapark-scrape.py
@@ -3,6 +3,8 @@ import asyncio
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urlunparse, parse_qs, urlencode, quote
 from requests_html import HTMLSession,AsyncHTMLSession
+from playwright.async_api import async_playwright
+
 def generate_mangapark_url(query=None,page=None,path=''):
     scheme = 'https'
     netloc = 'mangapark.io'
@@ -19,6 +21,27 @@ def generate_mangapark_url(query=None,page=None,path=''):
 
     return new_url
 
+# this will return list of title and link from query (default : 5 title)
+async def get_manga_titles(query):
+    try:
+        url = generate_mangapark_url(query=query,path='search')
+        # print(f"search url : {url}")
+        html = await get_html(url)
+        soup = BeautifulSoup(html,'lxml')
+        title_elements = soup.select('div[q\\:key="jp_1"] h3 a') # type: ignore
+        # 
+        result = []
+        for title_element in title_elements:
+            obj = {
+                title_element.get_text(strip=True) : generate_mangapark_url(path=title_element['href']) # type: ignore
+            }
+            # print(obj)
+            result.append(obj)
+        return result
+    except Exception as e:
+        raise e
+
+#get html from an url
 async def get_html(url):
     session = aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=10))
     try:
@@ -36,56 +59,69 @@ async def get_html(url):
     finally:
         await session.close()
 
-# this will return list of title and link from query (default : 5 title)
-async def get_manga_titles(query):
+# get list of volumes from a title 
+async def get_list_of_volume(url):
     try:
-        url = generate_mangapark_url(query=query,path='search')
-        print(url)
         html = await get_html(url)
         soup = BeautifulSoup(html,'lxml')
-        results = soup.select('div[q\\:key="jp_1"] h3 a') # type: ignore
-        for i in range(0,5):
-            print({
-                results[i].get_text(strip=True) : generate_mangapark_url(path=results[i]['href']) # type: ignore
-            })
-    except Exception as e:
-        raise e
-    
-async def get_list_of_volume(url):
-    html = await get_html(url)
-    soup = BeautifulSoup(html,'lxml')
-    table_datas = soup.select('a.link-hover.link-primary.visited\\:text-accent')
-    list_of_volume = {}
-    for data in table_datas:
-        list_of_volume[data.get_text(strip=True)] = data['href']
-    # print(list_of_volume)
-    return list_of_volume
-    
-async def get_chapter_data(url):
-    # html = await get_html(url)
-    # soup = BeautifulSoup(html,'lxml')
-    # manga_panel_main = soup.find('main')
-    # print(manga_panel_main)
-    # list_of_image = manga_panel_main.find_all('img') # type: ignore
-    # print(list_of_image)
-    session = AsyncHTMLSession()
-    r = await session.get(url) # type: ignore
-    page = await r.html.arender() # type: ignore
-    test = page.find('div[data-name\\:"image-item"]')
-    print(test)
+        table_datas = soup.select('a.link-hover.link-primary.visited\\:text-accent')
+        list_of_volume = {}
+        for data in table_datas:
+            list_of_volume[data.get_text(strip=True)] = data['href']
+        # print(list_of_volume)
+        return list_of_volume
+    except Exception:
+        print(f"Element of manga volume is not exist on {url}")
 
+# extract all link from manga page
+async def get_chapter_data(url, max_retries = 5):
+    max_retries = max_retries
+    for i in range(0, max_retries):
+        browser = None
+        async with async_playwright() as p:
+            try:
+                browser = await p.chromium.launch(headless=True)
+                page = await browser.new_page()
+                await page.goto(url)
+
+                # we could use for wait an element to appear or
+                # Alternatively, wait for network to be idle (no requests for 500ms)
+                # await page.wait_for_load_state('networkidle')
+                await page.wait_for_selector('div[data-name="image-item"]', timeout=8000)
+
+                html = await page.content()
+                
+                soup = BeautifulSoup(html, 'lxml')
+                main = soup.find('main')
+                img_urls = [img['src'] for img in main.find_all('img')] # type: ignore
+                # print(img_urls) 
+                return img_urls
+            except TimeoutError as e:
+                print(f"attemp {i + 1} timed out, trying again to render : {url}")
+                await asyncio.sleep(1)
+            except Exception:
+                print(f"unexpected error on attemp {i + 1} on {url}")
+                await asyncio.sleep(1)
+            finally:
+                if browser is not None:
+                    await browser.close()
     
+    raise Exception(f"failed to load {url} after {max_retries} tries")
+
+
 async def main():
-    html = await get_html("https://mangapark.io/search?word=domestic")
-    if html is None:
-         return
-    soup = BeautifulSoup(html,'lxml')
-    test = soup.select_one('div[q\\:key="jp_1"] h3 a').get('href')
-    print(test) # type: ignore
-    # titles = 
+    search_url : list[dict] = await get_manga_titles("domestic na")
+    main_url = list(search_url[0].values())[0] 
+    # print(main_url)
+    list_of_volume = await get_list_of_volume(main_url)
+    path = list(list_of_volume.values())[0] # type: ignore
+    chapter_url = generate_mangapark_url(path=path) # type: ignore
+    # print(chapter_url)
+    final_res = await get_chapter_data(chapter_url)
+    print(final_res)
+
 
 if __name__ == "__main__":
-    # asyncio.run(main())
-    # print(generate_mangapark_url(query="domestic na", path="search"))
-    # asyncio.run(get_list_of_volume("https://mangapark.io/title/388604-en-housekeeper-in-a-dungeon"))
-    asyncio.run(get_chapter_data("https://mangapark.io/title/54184-en-domestic-girlfriend/2469848-ch-277"))
+    asyncio.run(main())
+    # asyncio.run(get_chapter_data("https://mangapark.io/title/54184-en-domestic-girlfriend/2394832-ch-264"))
+    
